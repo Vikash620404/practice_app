9ebf5f954822670cfe6209cc8ebb5fc4d2205154
vikash kumar <vikashkumar620404800@gmail.com>
2022-04-05 04:52:42 +0530

    Vide_3Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("My first App",style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey,
-          fontFamily: 'IndieFlower',
-        ),),
-        centerTitle: true,
-        backgroundColor: Colors.red[600],
+  runApp(MaterialApp(home:Home()));
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "My first App",
+           
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.red[600],
+        ),
+        body: Center(
+          child: Text("Hello flutter",
+           style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey,
+              fontFamily: 'IndieFlower',
+            ),),
+          
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed:
+              () {}, //here i got error because onpressed is required here
+          child: Text('click'),
+          backgroundColor: Colors.black,
+        ),
       ),
-
-      body: Center(
-        child: Text("Hello flutter"),
-      ),
-
-      floatingActionButton: FloatingActionButton(  
-        onPressed: (){},   //here i got error because onpressed is required here
-        child: Text('click'),
-        backgroundColor: Colors.black,
-      ) ,
-    ),
-
-
-
-  ));
+    );
+  }
 }
