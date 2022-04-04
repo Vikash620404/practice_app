184498a0332f9cea556de7157ede92bc34e2a7b5
vikash kumar <vikashkumar620404800@gmail.com>
2022-04-05 05:15:33 +0530

    Completed video_4

--- lib/main.dart
+++ lib/main.dart
@@ -20,14 +20,7 @@ class Home extends StatelessWidget {
           backgroundColor: Colors.red[600],
         ),
         body: Center(
-          child: Text("Hello flutter",
-           style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey,
-              fontFamily: 'IndieFlower',
-            ),),
+          child: Image.asset('assets/image1.jpg'),
           
         ),
         floatingActionButton: FloatingActionButton(
