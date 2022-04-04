8a19230864293ef4deffa1caa0b4406ff59f5890
vikash kumar <vikashkumar620404800@gmail.com>
2022-04-05 03:44:29 +0530

    Video_2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,25 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Text("My first App"),
+        title: Text("My first App",style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey,
+          fontFamily: 'IndieFlower',
+        ),),
         centerTitle: true,
+        backgroundColor: Colors.red[600],
       ),
 
       body: Center(
         child: Text("Hello flutter"),
       ),
 
-      floatingActionButton: FloatingActionButton(     //here i got error
+      floatingActionButton: FloatingActionButton(  
+        onPressed: (){},   //here i got error because onpressed is required here
         child: Text('click'),
+        backgroundColor: Colors.black,
       ) ,
     ),
 
